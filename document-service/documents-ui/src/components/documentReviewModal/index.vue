c77ea6f1068cced3cb071f0cc428957f31723eda
Cameron Bowler <cameron.bowler@ca.ey.com>
2024-08-13 12:35:05 -0700

    relocate content to en.json and map class/types for review display

--- document-service/documents-ui/src/components/documentReviewModal/index.vue
+++ document-service/documents-ui/src/components/documentReviewModal/index.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-import { formatToReadableDate } from '../../utils/dateHelper'
+import { formatToReadableDate } from '~/utils/dateHelper'
 
 const { displayDocumentReview, documentInfoRO } = storeToRefs(useBcrosDocuments())
 const loadingDocuments = ref(false)
 
+/**
+ * Fetch and download the document record.
+ * Navigates to the Document Manage Page.
+ *
+ * @param consumerId - The consumer ID to fetch the document record for.
+ */
 const requestDocumentRecord = async (consumerId: string) => {
   loadingDocuments.value = true
   await getDocumentRecord(consumerId)
   loadingDocuments.value = false
   navigateTo({ name: RouteNameE.DOCUMENT_MANAGEMENT })
+}
+
+/**
+ * Retrieves the description based on the provided class or type value.
+ *
+ * @param value - The class or type value to look up.
+ * @param isType - Boolean indicating whether the value is a type or class. Defaults to false (class).
+ * @returns The description of the class or type if found, otherwise undefined.
+ */
+const getDescription = (value: string, isType = false): string | undefined => {
+  const docClass = documentTypes.find(docClass =>
+    isType
+      ? docClass.documents.some(doc => doc.type === value)
+      : docClass.class === value
+  )
+
+  return isType
+    ? docClass?.documents.find(doc => doc.type === value)?.description
+    : docClass?.description
 }
 </script>
 
@@ -28,48 +53,45 @@ const requestDocumentRecord = async (consumerId: string) => {
 
       <div class="pr-4">
         <div class="flex flex-row -mt-4">
-          <h3 class="text-[24px] font-bold">Document Information Saved</h3>
+          <h3 class="text-[24px] font-bold">{{ $t('documentReview.title') }}</h3>
         </div>
 
         <div class="flex flex-row py-6">
-          <span class="text-gray-700 font-normal text-[16px] leading-6">Your document information has been successfully
-            uploaded and saved. To view, complete a search on the document management page using the following
-            information
-          </span>
+          <span class="text-gray-700 font-normal text-[16px] leading-6">{{ $t('documentReview.description') }}</span>
         </div>
 
         <div class="grid grid-flow-row auto-rows-max">
           <div>
-            <span class="font-bold">Document ID: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.documentId') }}: </span>
             <span class="px-1">{{ documentInfoRO.consumerDocumentId }}</span>
           </div>
           <div>
-            <span class="font-bold">Entity ID: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.entityId') }}: </span>
             <span class="px-1">{{ documentInfoRO.consumerIdentifier || 'Not Entered' }}</span>
           </div>
           <div>
-            <span class="font-bold">Document Category: </span>
-            <span class="px-1">{{ documentInfoRO.documentClass }}</span>
+            <span class="font-bold">{{ $t('documentReview.labels.documentCategory') }}: </span>
+            <span class="px-1">{{ getDescription(documentInfoRO.documentClass) }}</span>
           </div>
           <div>
-            <span class="font-bold">Document Type: </span>
-            <span class="px-1">{{ documentInfoRO.documentType }}</span>
+            <span class="font-bold">{{ $t('documentReview.labels.documentType') }}: </span>
+            <span class="px-1">{{ getDescription(documentInfoRO.documentType, true) }}</span>
           </div>
           <div>
-            <span class="font-bold">Filing Date: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.filingDate') }}: </span>
             <span class="px-1">{{ formatToReadableDate(documentInfoRO.consumerFilingDateTime) || 'Not Entered' }}</span>
           </div>
 
           <div class="pt-8">
-            <span class="font-bold">Upload Date and Time: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.uploadDate') }}: </span>
             <span class="px-1">{{ formatToReadableDate(documentInfoRO.createDateTime) }}</span>
           </div>
           <div>
-            <span class="font-bold">Documents Uploaded: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.documentsUploaded') }}: </span>
             <span class="px-1">{{ documentInfoRO.filenames.length }}</span>
           </div>
           <div>
-            <span class="font-bold">Documents: </span>
+            <span class="font-bold">{{ $t('documentReview.labels.documentListLabel') }}: </span>
           </div>
           <div
             v-for="(filename, i) in documentInfoRO.filenames"
@@ -88,7 +110,7 @@ const requestDocumentRecord = async (consumerId: string) => {
             :loading="loadingDocuments"
             @click="navigateTo({ name: RouteNameE.DOCUMENT_MANAGEMENT })"
           >
-            Back to Documents Services Page
+            {{ $t('documentReview.buttonLabels.cancel') }}
           </UButton>
 
           <UButton
@@ -98,7 +120,7 @@ const requestDocumentRecord = async (consumerId: string) => {
             :loading="loadingDocuments"
             @click="requestDocumentRecord(documentInfoRO.consumerDocumentId)"
           >
-            Download Document Record Statement
+            {{ $t('documentReview.buttonLabels.downloadReport') }}
           </UButton>
         </div>
 
