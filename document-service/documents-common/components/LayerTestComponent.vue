2cf458c188be21994b3c43d39232c408f09d24b9
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2025-07-23 10:49:45 -0700

    Documents common framework (#178)
    * documents common layer framework
    
    * Updated Readme
    
    * Remove .nuxt and node_modules from repository and update .gitignore
    
    * include gitignore
    
    * minor config update

---
+++ document-service/documents-common/components/LayerTestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container bg-white p-4">
+    <h2>Playground Test Component</h2>
+    <p>{{ message }}</p>
+    <UButton @click="handleClick">Toggle Me</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const toggled = ref(false)
+const message = ref('Hello from PlaygroundTest!')
+function handleClick() {
+  toggled.value = !toggled.value
+  message.value = toggled.value
+    ? 'You toggled a button, yay!'
+    : 'Hello from PlaygroundTest!'
+}
+</script>
